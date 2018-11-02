6b361ec7235aac5dd6b94b2175a665d265bb3d9e
Athi Muthu <athidevwork@gmail.com>
2018-11-02 12:36:29 -0400

    latest sql

--- sql/showQQ.sql
+++ sql/showQQ.sql
@@ -12,6 +12,10 @@ col acct_to format a10
 col mt_rate_dt format a10
 col mt_acct_dt format a10
 col name format a15
+col quote_id format a15
+col rkey format a10
+col ckey format a10
+col prikey format a12
 
 accept quoteno char prompt "Enter Quote No: "
 spool H:\dev\policy\'&quoteno'.txt
@@ -19,14 +23,14 @@ spool H:\dev\policy\'&quoteno'.txt
 set termout off
 
 prompt ******* Quote Request Data
-select q.ws_qq_request_pk, q.quick_quote_id, q.quick_quote_generation_date, q.policy_type_code
+select q.ws_qq_request_pk prikey, q.quick_quote_id quote_id, q.quick_quote_generation_date, q.policy_type_code,
        q.start_date, q.end_date 
 from WS_QQ_Request q
 WHERE q.quick_quote_id = upper('&quoteno')
 /
 
 prompt ******* Quote Request Insured Data
-select q.ws_qq_insured_pk, q.quick_quote_id, q.risk_key, q.risk_type_code, q.risk_class_code, q.state_code,
+select q.ws_qq_insured_pk prikey, q.quick_quote_id quote_id, q.risk_key rkey, q.risk_type_code, q.risk_class_code, q.state_code,
        q.postal_code, q.postal_plus_four
 from WS_QQ_Insured q
 WHERE q.quick_quote_id = upper('&quoteno')
@@ -34,7 +38,7 @@ order by 1 desc
 /
 
 prompt ******* Quote Request Coverage Data
-select q.ws_qq_coverage_pk, q.quick_quote_id, q.risk_key, q.coverage_key, q.policy_form_code, 
+select q.ws_qq_coverage_pk prikey, q.quick_quote_id quote_id, q.risk_key rkey, q.coverage_key ckey, q.policy_form_code, 
        q.product_coverage_code, q.retroactive_date, q.coverage_limit_code, q.current_cm_step, q.premium_amount, 
        q.net_premium_amount
 from WS_QQ_Coverage q
@@ -43,7 +47,7 @@ order by 1 desc
 /
 
 prompt ******* Quote Request Component Data
-select q.ws_qq_component_pk, q.quick_quote_id, q.coverage_key, q.coverage_component_code, q.component_value,
+select q.ws_qq_component_pk prikey, q.quick_quote_id quote_id, q.coverage_key ckey, q.coverage_component_code, q.component_value,
        q.current_cm_step, q.premium_amount
 from WS_QQ_Component q
 WHERE q.quick_quote_id = upper('&quoteno')
